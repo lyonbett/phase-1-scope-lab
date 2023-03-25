dfaa5c8769ec14eafb90596c3c06a300eb7ba290
lyonbett <lyon.bett@student.moringaschool.com>
2023-03-25 20:40:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+ // declaring a variable using var
+ var customerName = 'bob' 
+  
+ function upperCaseCustomerName() { 
+    // modifying bob  
+     return customerName = customerName.toUpperCase(); 
+ } 
+  
+ // declaring a variable using const
+ const leastFavoriteCustomer = 'michelle' 
+  
+ function setBestCustomer(){ 
+     bestCustomer = 'not bob' 
+ } 
+  
+ function overwriteBestCustomer(){ 
+     bestCustomer = 'maybe bob' 
+ } 
+  
+ function changeLeastFavoriteCustomer(){ 
+     leastFavoriteCustomer = 'Michael' 
+ }
